6cede79e4ad5d5c117dc08f1065d9da64fae52cf
chewonit <chew_on_it@hotmail.com>
2013-05-29 14:09:05 +0800

    resolved a loading bug
    there was a bug when the user checks off a module to be marked as
    'taken' on some mobile browsers.
    shifted the loading div into the black overlay div.

--- wp-content/themes/eddiemachado-bones-6906c82/header.php
+++ wp-content/themes/eddiemachado-bones-6906c82/header.php
@@ -49,8 +49,7 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-		<div id="loading-overlay"></div>
-		<div id="loading-overlay-message"><center><h1>loading</h1><img src="<?php echo get_template_directory_uri(); ?>/library/images/loader.gif"></center></div>
+		<div id="loading-overlay"><div id="loading-overlay-message"><center><h1>loading</h1><img src="<?php echo get_template_directory_uri(); ?>/library/images/loader.gif"></center></div></div>
 		<div id="container">
 		
 			<header class="header" role="banner">
